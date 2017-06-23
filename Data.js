8108861cf40fe27c3aeb860c7ce36cd87e890ce3
Jens Van Herck <jensvanherck@gmail.com>
2017-06-24 01:57:18 +0200

    update

--- Data.js
+++ Data.js
@@ -1,3 +1,9 @@
-jsonCallBack({"backgrounds":[ "bg_anim_1", "5b84dfbe-3421-cf35-502a-eb2206d25ac3", "9ae8e9d4-54fa-8853-0658-65a644a8f010" ,"https://s5.postimg.org/9fh4jqggn/bg_anim_1.jpg",
+jsonCallBack({"backgrounds":[ 
+"bg_anim_1", "5b84dfbe-3421-cf35-502a-eb2206d25ac3", "9ae8e9d4-54fa-8853-0658-65a644a8f010" ,"https://s5.postimg.org/9fh4jqggn/bg_anim_1.jpg",
 "bg_anim_2", "a047de27-c3ce-61d3-9a3c-06c593834181", "51761bce-80a0-9d58-94be-f6ab17b4cb51" ,"https://s5.postimg.org/vddldivh3/bg_anim_2.jpg",
-"bg_anim_3", "968cb9bf-1b80-d31b-53c8-7b1d571ac88a", "bb9bde56-aedc-2735-58db-db49c4143e1e" ,"https://s5.postimg.org/wr588tuqf/bg_anim_3.jpg" ]});
+"bg_anim_3", "968cb9bf-1b80-d31b-53c8-7b1d571ac88a", "bb9bde56-aedc-2735-58db-db49c4143e1e" ,"https://s5.postimg.org/wr588tuqf/bg_anim_3.jpg" 
+"bg_anim_4", "75d237d2-982f-3e23-b602-8baeb1096c77", "ba4d0fe4-8fba-2299-99c4-57eee6ffbad4" ,"https://s5.postimg.org/fug5gen6f/bg_anim_4.jpg" 
+"bg_anim_5", "4f3ddbfe-ffda-4081-f707-9961a77e8ea7", "480f4874-cfc2-1c09-6312-0c1388d00bb7" ,"https://s5.postimg.org/ss80q063b/bg_anim_5.jpg" 
+"bg_anim_6", "727eba63-0ece-a7d1-691e-d8527c562ad1", "b071f490-f0c1-1ef8-4217-554e7a736840" ,"https://s5.postimg.org/4pr6v4pg7/bg_anim_6.jpg" 
+"bg_anim_7", "dcb5bd4d-5c8d-d296-88cf-761c3254ea95", "ac93b297-16a9-c284-8c3d-a2ab3c65c0be" ,"https://s5.postimg.org/fzjwq2uhj/bg_anim_7.jpg" 
+"bg_anim_8", "2644ae50-5d75-1d9b-54fb-696b56d3935d", "77462a17-f0d3-de39-42a8-46df7f769e05" ,"https://s5.postimg.org/pitljjhzr/bg_anim_8.jpg" ]});
